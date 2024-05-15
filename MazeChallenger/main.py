bae913c876491c16c3a6656876fa894f11c0c361
HNLuat <157623198+HNLuat@users.noreply.github.com>
2024-05-15 20:24:23 +0700

    Update main.py

--- MazeChallenger/main.py
+++ MazeChallenger/main.py
@@ -5,10 +5,13 @@ import random
 from maze import Maze
 from cell import Cell
 from monster import Monster
+from S_bullet import sbullet
+import math
 pygame.font.init()
 
+# các thông số chính của tựa game
 WIDTH, HEIGHT = 1200, 720
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Maze")
 black_BG = pygame.Rect(0, 0, WIDTH, HEIGHT) 
 BGmusic = pygame.mixer.music.load("My Game/audio/BGmusic2.mp3")
@@ -17,6 +20,7 @@ FONT = pygame.font.SysFont("comicsans", 30 )
 Mode = {"Easy": True, "Normal": False, "Hard": False, "Nightmare": False}
 
 class pause_table:
+    # các thuộc tính của bảng hiện lên khi dừng game
     def __init__(self, table_WIDTH, table_HEIGHT, table_thickness, element_WIDTH, element_HEIGHT, element_thickness, size ):
         self.Font_size = size
         self.Font =  pygame.font.SysFont("comicsans", size)
@@ -29,21 +33,35 @@ class pause_table:
         self.element_thickness = element_thickness
 
 def draw_BG():
-    # draw a dark background
+    # vẽ background
     pygame.draw.rect(WIN, "black", black_BG )
 
 def draw_button(button):
-    # draw "play game" button 
+    # vẽ nút "play game"
     pygame.draw.rect(WIN, "white", button, width = 5)
     play_text = FONT.render(f"PLAY GAME", 1, "white")
     WIN.blit(play_text, ( (WIDTH - 170)/2, (HEIGHT - 45)/2 ) )
 
-def draw_player(player):
-    # draw player
-    pygame.draw.rect(WIN, "white", player)
+def draw_player(player, monster):
+    # vẽ người chơi ,cây súng của người chơi và xoay cây súng theo hướng của kẻ địch
+    player_image = pygame.image.load("My Game/images/player.png")
+    player_image = pygame.transform.scale(player_image, (player.w, player.h))
+    gun_image = pygame.image.load("My Game/images/gun.png")
+    gun_image = pygame.transform.scale(gun_image, (player.w, player.h))
+    x_dist = monster.rect.centerx - player.centerx
+    y_dist = -(monster.rect.centery - player.centery)
+    angle = math.degrees(math.atan2(y_dist, x_dist))
+    center = (player.centerx + 10, player.centery - 2)
+    if angle > 90 or angle < -90:
+        gun_image = pygame.transform.flip(gun_image, flip_x = False, flip_y = True)
+        center =  (player.centerx - 10, player.centery - 2)
+    gun_image = pygame.transform.rotate(gun_image, angle)
+    gun_rect = gun_image.get_rect(center = center)
+    WIN.blit(player_image,(player.x, player.y))
+    WIN.blit(gun_image, gun_rect)
 
 def draw_rightside(num_key, num_key_to_win, num_bullet, max_bullet):
-    # draw current number of bullet and key
+    # vẽ cho người chơi biết số lượng key và bullet hiện có
     bullet = pygame.image.load("My Game/images/bullet.png")
     bullet = pygame.transform.scale(bullet, (75, 75))
 
@@ -59,11 +77,11 @@ def draw_rightside(num_key, num_key_to_win, num_bullet, max_bullet):
     WIN.blit(bullets_text,(1000, 300))
 
 def draw_pause_button(P_image):
-    # draw pause button
+    # vẽ nút dừng
     WIN.blit(P_image, (WIDTH - 300, 20))
 
 def draw_paused_table(P_table):
-    # draw paused table, player will see this after click pause button
+    # vẽ bảng khi dừng game
     ct = P_table.continue_table
     rt = P_table.return_table
     F = P_table.Font
@@ -78,7 +96,7 @@ def draw_paused_table(P_table):
     WIN.blit(return_text, (rt.x + round((rt.width - 0.63*6*sz)/2) , rt.y + round((rt.height - 1.5*sz)/2) ) ) 
 
 def draw_gameover(win):
-    # draw the "game over" text
+    # vẽ khi game đã kết thúc
     gameover_text = FONT.render(f"GAME OVER", 1, "white")
     WIN.blit(gameover_text, (450, 280))
     
@@ -89,13 +107,13 @@ def draw_gameover(win):
         youwin_text = FONT.render(f"YOU LOSE", 1, "red")
         WIN.blit(youwin_text, (465, 350))
 
-def draw_cell(cells, tile, obj1, obj1_size, obj2, obj2_size):
-    # draw walls betwween cells
+def draw_cell(cells, obj1, obj1_size, obj2, obj2_size):
+    # vẽ tường giữa những ô (cell)
     x, y = cells.x * cells.tile, cells.y * cells.tile
     if cells.obj1_here :
-        WIN.blit(obj1, (x + (tile - obj1_size)//2, y + (tile - obj1_size)//2) )
+        WIN.blit(obj1, (x + (cells.tile - obj1_size)//2, y + (cells.tile - obj1_size)//2) )
     if cells.obj2_here :
-        WIN.blit(obj2, (x + (tile - obj2_size)//2, y + (tile - obj2_size)//2) )
+        WIN.blit(obj2, (x + (cells.tile - obj2_size)//2, y + (cells.tile - obj2_size)//2) )
     if cells.walls['top']:
         pygame.draw.line(WIN, pygame.Color('darkgreen'), (x, y), (x + cells.tile, y), cells.thickness)
     if cells.walls['right']:
@@ -106,6 +124,7 @@ def draw_cell(cells, tile, obj1, obj1_size, obj2, obj2_size):
         pygame.draw.line(WIN, pygame.Color('darkgreen'), (x, y + cells.tile), (x, y), cells.thickness)
     
 def draw_Mode_button(easy_mode_button, normal_mode_button, hard_mode_button, nightmare_mode_button):
+    # vẽ nút chọn độ khó và hiển thị độ khó hiện tại
     pygame.draw.rect(WIN, "green", easy_mode_button, width = 10)
     pygame.draw.rect(WIN, "orange", normal_mode_button, width = 10)
     pygame.draw.rect(WIN, "red", hard_mode_button, width = 10)
@@ -130,18 +149,17 @@ def draw_Mode_button(easy_mode_button, normal_mode_button, hard_mode_button, nig
 
 
 def get_current_cell(x, y, grid_cells):
-    # get cell (x,y) in grid_cells 
+    # trả vè ô (cell) ở vị trí (x, y), ( (x, y) là vị trí của ô đó )
     for cell in grid_cells:
         if cell.x == x and cell.y == y:
             return cell
         
 def check_move(x, y, grid_cells, tile, thickness, moving_state):
-    # check if the player go through wall
+    # kiểm tra xem người chơi có di chuyển đụng tường hay ra ngoài map không
     current_cell_x, current_cell_y = x // tile, y // tile
     current_cell = get_current_cell(current_cell_x, current_cell_y, grid_cells)
     current_cell_abs_x, current_cell_abs_y = current_cell_x * tile, current_cell_y * tile
     temp_moving_state = moving_state
-    # change the moving state
     if moving_state['left']:
         if current_cell.walls['left']:
             if x <= current_cell_abs_x + thickness :
@@ -161,7 +179,7 @@ def check_move(x, y, grid_cells, tile, thickness, moving_state):
     return temp_moving_state
 
 def check_move2(direction, vel, x1, x2, y, grid_cells, tile):
-    # check if the player move betwwen wall
+    # kiểm tra xem người chơi có di chuyển vào giữa tường (wall) không
     current_cell_x1, current_cell_x2, current_cell_y = x1 // tile, x2 // tile, y // tile
     if direction == "left" or direction == "right" :
         current_cell_1 = get_current_cell(current_cell_y, current_cell_x1, grid_cells)
@@ -181,7 +199,7 @@ def check_move2(direction, vel, x1, x2, y, grid_cells, tile):
     return 1
 
 def MOVE(keys, player, vel, grid_cells, tile, thickness):
-    # move the player
+    # hàm di chuyển người chơi
     moving_state = {'left' : keys[pygame.K_LEFT], 'right' : keys[pygame.K_RIGHT], 'up' : keys[pygame.K_UP], 'down' : keys[pygame.K_DOWN] }
     temp_state = check_move(player.x, player.y, grid_cells, tile, thickness, moving_state)
     temp_state = temp_state and check_move(player.x + player.w, player.y, grid_cells, tile, thickness, moving_state)
@@ -199,7 +217,8 @@ def MOVE(keys, player, vel, grid_cells, tile, thickness):
     
 
 def check_eat_obj(player, maze):
-    # check if the player get the bullet or the key
+    # kiểm tra xem người chơi có nhặt key hay bullet không
+    # hàm sẽ trả về 1 tuple(x, y) (x, y theo kiểu bool) x = 1 thì người chơi nhặt được key, y = 1 thì người chơi nhặt được bullet
     eat1 = 0
     eat2 = 0
     for cell in maze.grid_cells :
@@ -214,7 +233,7 @@ def check_eat_obj(player, maze):
     return eat1, eat2
 
 def reached_goal(player, goal_cell, tile):
-    # check if the player reach goal cell
+    # kiểm tra xem người chơi đã đến đích chưa
     goal_cell_abs_x, goal_cell_abs_y = goal_cell.x * tile, goal_cell.y * tile
     if player.x >= goal_cell_abs_x and player.y >= goal_cell_abs_y:
         return True
@@ -222,20 +241,18 @@ def reached_goal(player, goal_cell, tile):
         return False
 
 def check_choose_mode(mouse_pos, easy_mode_button, normal_mode_button, hard_mode_button, nightmare_mode_button):
-    # check if the player choose game mode
+    # kiểm tra xem người chơi có chọn chế độ chơi không, và thay đổi chế độ chơi
     easy = easy_mode_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
     normal = normal_mode_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
     hard = hard_mode_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
     nightmare = nightmare_mode_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
 
-    # if player has click at any choosing game mode button, we init all the Mode to false,
     if easy or normal or hard or nightmare:
         Mode["Easy"] = False
         Mode["Normal"] = False
         Mode["Hard"] = False
         Mode["Nightmare"] = False
 
-    # then check if which one is clicked
     if (easy_mode_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]):
         Mode["Easy"] = True
     if (normal_mode_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]):
@@ -246,94 +263,82 @@ def check_choose_mode(mouse_pos, easy_mode_button, normal_mode_button, hard_mode
         Mode["Nightmare"] = True
 
 def main():
+    # chạy vòng lặp game
     inHub = True
     
-    # "Play game" button stats
     button_WIDTH = 200
     button_HEIGHT = 60  
 
-    # create gamemode button
     easy_mode_button = pygame.Rect(0, HEIGHT - 100, 300, 200)
     normal_mode_button = pygame.Rect(300, HEIGHT - 100, 300, 200)
     hard_mode_button = pygame.Rect(600, HEIGHT - 100, 300, 200)
     nightmare_mode_button = pygame.Rect(900, HEIGHT - 100, 300, 200)
 
-    # "pause" button stat
     P_button = pygame.Rect(WIDTH - 300, 20, 100, 100)
     P_image = pygame.image.load("My Game/images/pause_button.png")
     P_image = pygame.transform.scale(P_image, (100, 100))
     P_table = pause_table(400, 300, 5, 300, 80, 5, 40)
     paused = False
 
-    # to check if the game is over
-    # gameover=0: not over yet
-    # gameover=1: player win
-    # gameover=-1: player lose
     gameover = 0
 
-    # player stats
-    player_size = 25
+    player_size = 40
     player = pygame.Rect(4, 4, player_size, player_size)
     player_VEL = 5
     
     run = True
     clock = pygame.time.Clock()
     
-    # time couting
     elapsed_time = 0
     start_time = time.time()
     paused_time = 0
+    last_shoot_time = time.time()
     
-    # bullet stats
-    bullet_size = 30
-    bullet_add_increment = 5000
-    num_bullet_add = 2
-    bullet_count = 7000
+    bullet_size = 25
+    bullet_add_increment = 3000
+    num_bullet_add = 1
+    bullet_count = 3000
     bullet = pygame.image.load("My Game/images/bullet.png")
     bullet = pygame.transform.scale(bullet, (bullet_size, bullet_size))
     max_bullet = 12
     num_bullet = 6
+    
+    bullet_vel = 20
+    shooted_bullet_size = 20
+    list_shooted_bullet = []
 
-    # key stats
-    key_size = 25
+    key_size = 30
     key = pygame.image.load("My Game/images/key.png")
-    bullet = pygame.transform.scale(bullet, (bullet_size, bullet_size))
+    key = pygame.transform.scale(key, (key_size, key_size))
     num_key = 0
     num_key_to_win = 10
 
-    # check if the game start and init stuff
     game_init = 0
 
-    # maze stats
-    tile = 45
+    tile = 60
     maze_size = HEIGHT//tile
     total_grid = maze_size**2
     maze = Maze(maze_size, maze_size, tile, bullet_size, key_size)
     ids_list = []
     for i in range(total_grid):
         ids_list.append(i)
-    # generate_maze
     maze.generate_maze()
 
-    # monster stats
-    monster = Monster(5, 25)
+    monster = Monster(5, 40)
     monster_star_running_time = 4
     monster_paralyzed_time = 4
     monster_is_paralyzed = False
     paralyzed_count = 0
-
     while run:
         clock.tick(50)
         mouse_pos = pygame.mouse.get_pos()
 
-        # get event and keys that player pressed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
         keys = pygame.key.get_pressed()
 
-        # when we are in main lobby
         if inHub:
             button = pygame.Rect(( (WIDTH - button_WIDTH)/2, (HEIGHT - button_HEIGHT)/2 ), (button_WIDTH, button_HEIGHT))
             if (button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]) or ((time.time() - start_time) >=1 and keys[pygame.K_SPACE]):
@@ -346,7 +351,6 @@ def main():
             pygame.display.update()
             continue 
         
-        # when the game is over
         if gameover != 0 :
             draw_gameover(gameover)
             pygame.display.update()
@@ -356,7 +360,6 @@ def main():
                 return
             continue
 
-        # when the game is pause
         if paused:
             paused_time = time.time() - start_time - elapsed_time
             draw_paused_table(P_table)
@@ -368,53 +371,48 @@ def main():
             pygame.display.update()
             continue
 
-        # check if we pause
         if (P_button.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]) or keys[pygame.K_p]:
             paused = True
 
-        # count time, bullet and get the key the player press 
         bullet_count += 20
         elapsed_time = time.time() - start_time - paused_time
         
-        # initilize stuff in game
         if not game_init:
-            # Game mode
+            last_shoot_time = time.time()
             if Mode["Normal"]:
                 max_bullet = 6
                 num_bullet = 3
-                monster.vel = 7
+                monster.vel = 6
                 monster.color = "orange"
+                monster_star_running_time = 3.5
+                monster_paralyzed_time = 3.5
+                bullet_add_increment = 3500
             if Mode["Hard"]:
                 max_bullet = 6
                 num_bullet = 3
-                monster.vel = 9
+                monster.vel = 8
                 monster.color = "red"
                 monster_star_running_time = 3
                 monster_paralyzed_time = 3
+                bullet_add_increment = 4000
             if Mode["Nightmare"]:
                 max_bullet = 6
                 num_bullet = 3
-                monster.vel = 15
+                monster.vel = 10
                 monster.color = "purple"
-                monster_star_running_time = 2
-                monster_paralyzed_time = 2
-            # Spawn key
+                monster_star_running_time = 2.5
+                monster_paralyzed_time = 2.5
+                bullet_add_increment = 4500
+                bullet_count = 0
             random.shuffle(ids_list)
             for _ in range(num_key_to_win):
                 i = ids_list[_]
                 maze.grid_cells[i].obj2_here = 1
             game_init = 1
         
-        # check if player shoot
-        if keys[pygame.K_SPACE] and num_bullet > 0 and not monster_is_paralyzed:
-            num_bullet -= 1
-            paralyzed_count = elapsed_time
-        if elapsed_time - paralyzed_count <= monster_paralyzed_time :
-            monster_is_paralyzed = True
-        else :
+        if elapsed_time - paralyzed_count >= monster_paralyzed_time:
             monster_is_paralyzed = False
 
-        # check if the game is over
         if not monster_is_paralyzed and elapsed_time > monster_star_running_time:
             if monster.run(player, maze) == 1:
                 gameover = -1
@@ -422,8 +420,6 @@ def main():
         if num_key == num_key_to_win and reached_goal(player, maze.grid_cells[-1], tile):
             gameover = 1
         
-
-        # add more bullet
         if bullet_count >= bullet_add_increment:
             random.shuffle(ids_list)
             num = 0
@@ -436,23 +432,42 @@ def main():
                         break
             bullet_count = 0
                     
-        # move the player
-        keys = pygame.key.get_pressed()
-        
         MOVE(keys, player, player_VEL, maze.grid_cells, tile, maze.thickness)
 
-        # check if the player pick the bullet and the key
         eat_state = check_eat_obj(player, maze)
-        if num_bullet < max_bullet:
+        if num_bullet < max_bullet: 
             num_bullet += eat_state[0]
         num_key += eat_state[1]
 
-        # draw and display
         draw_BG()
-        draw_player(player)
+        draw_player(player, monster)
         monster.draw(WIN)
         for cell in maze.grid_cells:
-            draw_cell(cell, tile, bullet, bullet_size, key, key_size)
+            draw_cell(cell, bullet, bullet_size, key, key_size)
+        
+        if len(list_shooted_bullet) > 0:
+            lst_pop = []
+            for i in range(len(list_shooted_bullet)):
+                s_bullet = list_shooted_bullet[i]
+                flag = False
+                if s_bullet.run(WIN, HEIGHT) == 0:
+                    flag = True
+                if s_bullet.rect.colliderect(monster.rect):
+                    monster_is_paralyzed = True
+                    paralyzed_count = elapsed_time
+                    flag = True
+                if flag:
+                    lst_pop.append(i)
+            for i in lst_pop:
+                list_shooted_bullet.pop(i)
+
+        if keys[pygame.K_SPACE] and num_bullet > 0 and time.time() - last_shoot_time >= 0.5:
+            shooted_bullet = sbullet(bullet_vel, shooted_bullet_size)
+            list_shooted_bullet.append(shooted_bullet)
+            shooted_bullet.shoot(WIN, player.centerx, player.centery, monster.rect.centerx, monster.rect.centery)
+            last_shoot_time = time.time()
+            num_bullet -= 1
+
         draw_pause_button(P_image)
         draw_rightside(num_key, num_key_to_win, num_bullet, max_bullet)
         pygame.display.update()
